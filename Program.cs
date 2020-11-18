6f90cafde2f12722d5c74f728958ff02a08665b6
anonyblast <gustavoiafelix@gmail.com>
2020-11-18 16:55:00 -0300

    Hello World em C#!

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace DigitalInnovationOne
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numeroDeVezes = 5;
+
+            for (int i = 0; i < numeroDeVezes; i++) //Laço de repetição
+            {
+                Console.WriteLine($"Bem-Vindo ao curso de .Net {i}");
+                Console.WriteLine("Hello World!"); 
+            }
+        }
+    }
+}
